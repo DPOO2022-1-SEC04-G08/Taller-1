6649f90b57242a824d3f90e3f3ac7a3ab3d76534
acastem15 <a.castellanosm@uniandes.edu.co>
2022-02-06 20:10:34 -0500

    Clase pedido version1

---
+++ src/uniandes/dpoo/taller1/modelo/Pedido.java
@@ -0,0 +1,126 @@
+package uniandes.dpoo.taller1.modelo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Pedido {
+	// Attributes
+	//private int numeroPedidos;
+	private int idPedido;
+	private String nombreCliente; 
+	private String direccionCliente; 
+	
+	private  ArrayList<Producto> itemsPedido;  
+	
+	/**
+	 * @param nombreCliente
+	 * @param direccionCliente
+	 */
+	//Constructor
+	public Pedido(String nombreCliente, String direccionCliente) {
+		this.nombreCliente = nombreCliente; 
+		this.direccionCliente = direccionCliente;
+	}
+	
+	// Get methods
+	
+	public int getIdPedido() {
+		return idPedido;
+	}
+	 
+	//Precio neto antes de impuestos
+	// Preguntar si se deberian hacer estos metodos float para casos donde 
+	//el precio o el impuesto no es entero?
+	private int getPrecioNetoPedido() {
+		int PrecioNeto = 0;
+		
+		
+		for (Producto producto: itemsPedido) {
+			
+			PrecioNeto += producto.getPrecio();
+		}
+		return PrecioNeto;
+		
+	}
+	//Impuestos
+	private int getPrecioIVAPedido() {
+		
+		int IVA = (getPrecioTotalPedido()*(19))/100;
+		
+		return IVA;
+		
+		
+	}
+	
+	//Precio total despues de impuestos
+	private int getPrecioTotalPedido() {
+		int neto = getPrecioNetoPedido();
+		int impuestos = getPrecioIVAPedido();
+		
+		int PrecioTotal = neto + impuestos; 
+		
+		return PrecioTotal;
+	}
+	
+	//Methods
+	
+	public void agregarProducto(Producto nuevoItem) {
+		itemsPedido.add(nuevoItem);
+		
+	}
+	
+	private String generarTextoFactura() {
+		// Pregunta: Se debe poner nombre del cliente?
+		String FacturaText = "Factura; Id del Pedido: " + String.valueOf(idPedido) + "; Precio por producto: ;";
+		
+		
+				
+		for (Producto producto: itemsPedido ) {
+			
+			String nameProducto = producto.getNombre();
+			int precioProducto = producto.getPrecio();
+			
+			String format = nameProducto + "    " + String.valueOf(precioProducto)+";"; 
+			FacturaText += format; 
+			
+		}
+		FacturaText += ";" + ";"+ ";";
+		FacturaText += "Precio neto: " + String.valueOf(getPrecioNetoPedido())+ ";";
+		FacturaText += "IVA: " + String.valueOf(getPrecioIVAPedido())+ ";";
+		FacturaText += "Precio Total: " + String.valueOf(getPrecioTotalPedido())+ ";";
+		return FacturaText; 
+		
+	}
+	// Preguntar si el archivo nos es dado por parametro o se crea desde 0 el archivo 
+	public void guardarFactura(File archivo) throws IOException {
+		//El formato del archi ha de ser ./Facturas/archivo.txt para que se guarde en la carpeta Facturascfvvvvvvvvvvvvvvvvbv g32
+		// Creacion de directorio (carpeta Facturas) dentro del proyecto
+		File directorio = new File("./Facturas");
+		
+		if ( !directorio.exists()) {
+			directorio.mkdirs();
+		}
+		
+		// Escribir el texto generado en la otra funcion dentro del archivo de parámetro
+		//Ojo al hacer restaurante incluir los tipos de archivo ./Facturas/archivo.txt
+		//para que se guarde en facturas
+		
+		 FileWriter escribir = new FileWriter(archivo, true);
+		 String[] facturaPartida = generarTextoFactura().split(";"); 
+		 for (String linea:facturaPartida) {
+			 //Imprimir linea y salto de linea
+			 escribir.write(linea);
+			 escribir.write(System.getProperty( "line.separator" ));
+			 
+		 }
+		 
+		 escribir.close();
+
+
+		
+	}
+
+}
