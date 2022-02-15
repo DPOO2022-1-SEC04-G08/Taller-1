fd0ce6cba0ba1642721e75b8e5f9fb82b8c6eac4
gordosaurio <63026665+gordosaurio@users.noreply.github.com>
2022-02-15 10:17:06 -0500

    metodos de apoyo en la interface

--- src/uniandes/dpoo/taller1/consola/Aplicacion.java
+++ src/uniandes/dpoo/taller1/consola/Aplicacion.java
@@ -18,7 +18,7 @@ public class Aplicacion {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Aplicacion consola = new Aplicacion();
 		//Pruebas//
-		consola.probarcarga();
+		//consola.probarcarga();
 		//Ejecutar app
 		consola.ejecutarAplicacion();
 
@@ -53,6 +53,8 @@ public class Aplicacion {
 	{
 		// insertar la carga de datos aqui 
 		boolean continuar = true;
+		//String nombre = input("ingrese su nombre: ");
+		//String direccion = input ("ingrese su direccion: ");
 		while (continuar)
 		{
 			mostrarMenu();
@@ -89,7 +91,7 @@ public class Aplicacion {
 				System.out.println("cerrando programa");
 				continuar = false;
 			}
-			System.out.println(opcion_seleccionada);
+			
 		}
 	}
 	
@@ -124,9 +126,239 @@ public class Aplicacion {
 		//insertar el menu 
 	}
 
-
-
+	//private void nuevoPedido()
+	//{
+	//	String productos = new String("");
+	//	int numeroProductos = Integer.parseInt(input("Por favor seleccione una opcion"));
+	//	int contador = 0;
+	//	while (contador < numeroProductos)
+	//	{
+	//		if (contador == (numeroProductos - 1))
+	//		{
+	//			string producto = input("inserte el producto")
+	//			productos = productos+ 
+	//		}
+	//	}
+	//}
 	
+	//para que nos de el nombre del combo
+	
+	private String nombreCombo ()
+	{
+		String respuesta = new String("");
+		System.out.print("1. combo corral \n");
+		System.out.print("2. combo corral queso\n");
+		System.out.print("3. combo todoterreno\n");
+		System.out.print("4. combo especial\n");
+		
+		int opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion"));
+		boolean hasta = false;
+		if ((opcion_seleccionada == 0) || ( opcion_seleccionada > 4)){
+			hasta = true;
+		}
+		while (hasta) {
+			opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion valida"));
+			if (( opcion_seleccionada != 0) && (opcion_seleccionada < 5)) {
+				hasta = false;
+			}
+		}
+		
+		if (opcion_seleccionada == 1) {
+			respuesta = "combo corral";
+		}
+		else if (opcion_seleccionada == 2) {
+			respuesta = "combo corral queso";
+		}
+		else if (opcion_seleccionada == 3) {
+			respuesta = "combo todoterreno";
+		}
+		else if (opcion_seleccionada == 4) {
+			respuesta = "combo especial";
+		}
+		return respuesta;
+	}
+	
+	private String nombreMenu ()
+	{
+		System.out.print("1. corral \n");
+		System.out.print("2. corral queso \n");
+		System.out.print("3. corral pollo \n");
+		System.out.print("4. corralita \n");
+		System.out.print("5. todoterreno \n");
+		System.out.print("6. 1/2 libra \n");
+		System.out.print("7. especial \n");
+		System.out.print("8. casera \n");
+		System.out.print("9. mexicana \n");
+		System.out.print("10. criolla \n");
+		System.out.print("11. costeña \n");
+		System.out.print("12. hawaiana \n");
+		System.out.print("13. wrap de pollo \n");
+		System.out.print("14. wrap de lomo \n");
+		System.out.print("15. ensalada mexicana \n");
+		System.out.print("16. papas medianas \n");
+		System.out.print("17. papas grandes \n");
+		System.out.print("18. papas en casco medianas \n");
+		System.out.print("19. papas en casco grandes \n");
+		System.out.print("20. agua cristal sin gas \n");
+		System.out.print("21. agua cristal con gas \n");
+		System.out.print("22. gaseosa \n");
+		String respuesta = new String("");
+		int opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion"));
+		boolean hasta = false;
+		if ((opcion_seleccionada == 0) || ( opcion_seleccionada > 22)){
+			hasta = true;
+		}
+		while (hasta) {
+			opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion valida"));
+			if (( opcion_seleccionada != 0) && (opcion_seleccionada < 23)) {
+				hasta = false;
+			}
+		}
+		if (opcion_seleccionada == 1) {
+			respuesta = "corral";
+		}
+		else if (opcion_seleccionada == 2) {
+			respuesta = "corral queso";
+		}
+		else if (opcion_seleccionada == 3) {
+			respuesta = "corral pollo";
+		}
+		else if (opcion_seleccionada == 4) {
+			respuesta = "corralita";
+		}
+		else if (opcion_seleccionada == 5) {
+			respuesta = "todoterreno";
+		}
+		else if (opcion_seleccionada == 6) {
+			respuesta = "1/2 libra";
+		}
+		else if (opcion_seleccionada == 7) {
+			respuesta = "especial";
+		}
+		else if (opcion_seleccionada == 8) {
+			respuesta = "casera";
+		}
+		else if (opcion_seleccionada == 9) {
+			respuesta = "mexicana";
+		}
+		else if (opcion_seleccionada == 10) {
+			respuesta = "criolla";
+		}
+		else if (opcion_seleccionada == 11) {
+			respuesta = "costeña";
+		}
+		else if (opcion_seleccionada == 12) {
+			respuesta = "hawaiana";
+		}
+		else if (opcion_seleccionada == 13) {
+			respuesta = "wrap de pollo";
+		}
+		else if (opcion_seleccionada == 14) {
+			respuesta = "wrap de lomo";
+		}
+		else if (opcion_seleccionada == 15) {
+			respuesta = "ensalada mexicana";
+		}
+		else if (opcion_seleccionada == 16) {
+			respuesta = "papas medianas";
+		}
+		else if (opcion_seleccionada == 17) {
+			respuesta = "papas grandes";
+		}
+		else if (opcion_seleccionada == 18) {
+			respuesta = "papas en casco medianas";
+		}
+		else if (opcion_seleccionada == 19) {
+			respuesta = "papas en casco grandes";
+		}
+		else if (opcion_seleccionada == 20) {
+			respuesta = "agua cristal sin gas";
+		}
+		else if (opcion_seleccionada == 21) {
+			respuesta = "agua cristal con gas";
+		}
+		else if (opcion_seleccionada == 22) {
+			respuesta = "gaseosa";
+		}
+		return respuesta;
+	}
+
+	private String ingrediente ()
+	{
+		System.out.print("1. lechuga \n");
+		System.out.print("2. tomate \n");
+		System.out.print("3. cebolla \n");
+		System.out.print("4. queso mozzarella \n");
+		System.out.print("5. huevo \n");
+		System.out.print("6. queso americano \n");
+		System.out.print("7. tocineta express \n");
+		System.out.print("8. papa callejera \n");
+		System.out.print("9. pepinillos \n");
+		System.out.print("10. cebolla grille \n");
+		System.out.print("11. suero costeño \n");
+		System.out.print("12. frijol refrito \n");
+		System.out.print("13. queso fundido \n");
+		System.out.print("14. tocineta picada \n");
+		System.out.print("15. piña \n");
+		String respuesta = new String("");
+		int opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion"));
+		boolean hasta = false;
+		if ((opcion_seleccionada == 0) || ( opcion_seleccionada > 15)){
+			hasta = true;
+		}
+		while (hasta) {
+			opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion valida"));
+			if (( opcion_seleccionada != 0) && (opcion_seleccionada < 16)) {
+				hasta = false;
+			}
+		}
+		if (opcion_seleccionada == 1) {
+			respuesta = "lechuga";
+		}
+		else if (opcion_seleccionada == 2) {
+			respuesta = "tomate";
+		}
+		else if (opcion_seleccionada == 3) {
+			respuesta = "cebolla";
+		}
+		else if (opcion_seleccionada == 4) {
+			respuesta = "queso mozzarella";
+		}
+		else if (opcion_seleccionada == 5) {
+			respuesta = "huevo";
+		}
+		else if (opcion_seleccionada == 6) {
+			respuesta = "queso americano";
+		}
+		else if (opcion_seleccionada == 7) {
+			respuesta = "tocineta express";
+		}
+		else if (opcion_seleccionada == 8) {
+			respuesta = "papa callejera";
+		}
+		else if (opcion_seleccionada == 9) {
+			respuesta = "pepinillos";
+		}
+		else if (opcion_seleccionada == 10) {
+			respuesta = "cebolla grille";
+		}
+		else if (opcion_seleccionada == 11) {
+			respuesta = "suero costeño";
+		}
+		else if (opcion_seleccionada == 12) {
+			respuesta = "frijol refrito";
+		}
+		else if (opcion_seleccionada == 13) {
+			respuesta = "queso fundido";
+		}
+		else if (opcion_seleccionada == 14) {
+			respuesta = "tocineta picada";
+		}
+		else if (opcion_seleccionada == 15) {
+			respuesta = "piña";
+		}
+		return respuesta ;
+	}
 	
 	public String input(String mensaje)
 	{
